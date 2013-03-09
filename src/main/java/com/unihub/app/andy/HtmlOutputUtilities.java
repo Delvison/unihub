2a69da9a1b669336251c194c8dc455e20fe2ea26
Andy Valdez <avaldez@oswego.edu>
2013-03-08 21:18:29 -0500

    added nice index img

---
+++ src/main/java/com/unihub/app/andy/HtmlOutputUtilities.java
@@ -0,0 +1,8 @@
+/*********************************************
+* utility class used to dynamically generate *
+* html like for dynamic <title> tag etc.	 *
+*********************************************/
+public class HtmlOutputUtilities {
+
+
+}//end of class
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,6 +2,10 @@
 <%@ include file="/delvison/header.jsp" %>
 
 <body>
+
+	<div class='span12 main' style="background-color:White">
+		<img src="design/images/y_u_no.jpg">
+	</div>
 	
 
 </body>
