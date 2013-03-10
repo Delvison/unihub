c56e34a18189b4abcec16875288442e88755edb2
Andy Valdez <avaldez@oswego.edu>
2013-03-10 14:35:36 -0400

    made categories list print out in index

--- src/main/java/com/unihub/app/andy/HtmlOutputUtilities.java
+++ src/main/java/com/unihub/app/andy/HtmlOutputUtilities.java
@@ -2,7 +2,33 @@
 * utility class used to dynamically generate *
 * html like for dynamic <title> tag etc.	 *
 *********************************************/
+package com.unihub.app;
+
 public class HtmlOutputUtilities {
+
+	public static String bootStrapRow(String mario, String luigi) {
+
+		return "<div class=\"row-fluid\">"+
+					"<div class=\"span4\" style=\"background-color:White\">" +
+						"<h3>"+mario+"</h3>"+
+					"</div>"+
+					"<div class=\"span4\" style=\"background-color:White\">" +
+						"<h3>"+luigi+"</h3>"+
+					"</div>"+
+				"</div>";
+
+	}//end of bootStrapRow
+
+	public static String bootStrapRow(String mario) {
+
+		return "<div class=\"row-fluid\">"+
+					"<div class=\"span4\" style=\"background-color:White\">" +
+						"<h3>"+mario+"</h3>"+
+					"</div>"+
+				"</div>";
+		
+
+	}//end of bootStrapRow
 
 
 }//end of class
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,12 +1,39 @@
 <!--This shall be our main home screen -->
+<%@ page import="com.unihub.app.HtmlOutputUtilities" %>
+<%
+	String[] cats = new String[] {"Textbooks", "Jobs", "Electronics", "Cars"};
+	int counter = 0;
+%>
+
+
+
 <%@ include file="/delvison/header.jsp" %>
 
 <body>
+	<div class="container-fluid">
 
-	<div class='span12 main' style="background-color:White">
-		<img src="design/images/y_u_no.jpg">
+
+
+		<div class="hero-unit" style="background-color:White">
+			<img src="design/images/y_u_no.jpg">
+		</div>
+
+
+		
+
+			<% for(int i = 0; i < cats.length; i++) {
+				if(i % 2 == 0) { %>
+
+					<%= HtmlOutputUtilities.bootStrapRow(cats[i], cats[i+1]) %>
+			<%
+				}
+
+			} %>
+
+
+
+
 	</div>
-	
 
 </body>
 
